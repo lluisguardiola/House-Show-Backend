608ad7feb4585fdc02423660ea2845533280144f
lluisguardiola <lluisguardiola.h@gmail.com>
2020-01-21 19:08:25 -0600

    initial render of an event's respective shows

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
 	def index
 		events = Event.all
-        render json: events, include: [:neighborhood, :shows]
+        render json: events, include: [:neighborhood]
 	end
+
+	def show
+        event = Event.find_by(id: params[:id])
+        render json: event.shows, include: :performer
+    end
 end
