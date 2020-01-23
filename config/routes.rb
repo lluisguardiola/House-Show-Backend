476ad4cceeda456d78af78fa8e1cee076aade47e
lluisguardiola <lluisguardiola.h@gmail.com>
2020-01-23 11:28:27 -0600

    newShowForm feature done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :neighborhoods, only: :index
   resources :performers, only: :index
   resources :events, only: [:index, :show]
+  resources :shows, only: [:create]
 end
